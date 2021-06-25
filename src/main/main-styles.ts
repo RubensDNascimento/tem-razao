30a3467c4ce524e4a823abbe0250a5a7e044a88a
Philipe Loureiro <philipeloureiro@gmail.com>
2021-06-25 10:05:50 -0300

    Feat: Lego Component

---
+++ src/main/main-styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  background-color: #481658;
+  width: 100vw;
+  height: 100vh;
+  justify-content: space-evenly;
+  align-items:  center;
+`;
