271c92fb0e20a2e0ca76491b5c3d9bf89c927f91
Rubens D Nascimento <rubensd.nascimento@yahoo.com.br>
2021-07-06 19:11:08 -0300

    page: Sobre

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import Main from './screens/main/main';
+import Sobre from './screens/sobre/sobre';
 
 function App() {
-  return <Main />;
+  return <Sobre />;
 }
 
 export default App;
